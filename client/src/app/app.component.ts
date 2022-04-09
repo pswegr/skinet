9a452f217a4dd2d5c68e5b6f3286daff902aeea6
Pawel W <pswegr@hotmail.com>
2022-04-09 12:03:48 +0200

    End of section 8 - app-component update

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit{
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:7107/api/products?pageSize=50').subscribe((response: IPagination) => {
+    this.http.get('https://localhost:7107/api/products?pageSize=50').subscribe((response: any) => {
       this.products = response.data;
     }, error => {
       console.log(error);
